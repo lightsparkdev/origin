ce30e46d9c2637d24df9ecc20d73606ca1a9e4ea
thisisjaymantri <124090453+thisisjaymantri@users.noreply.github.com>
2026-01-22 13:21:09 -0800

    Add barrel export for package consumption
    - Create src/index.ts with all component exports
    - Rename package to @grid/origin
    - Add main/exports fields for local package resolution
    - Add React peer dependencies

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+// Origin Design System - Barrel Export
+// This file exports all components for use as a package
+
+// Compound components (namespace exports)
+export { Accordion } from './components/Accordion';
+export { AlertDialog } from './components/AlertDialog';
+export { Autocomplete } from './components/Autocomplete';
+export { Breadcrumb } from './components/Breadcrumb';
+export { Checkbox } from './components/Checkbox';
+export { Combobox } from './components/Combobox';
+export { ContextMenu } from './components/ContextMenu';
+export { Field } from './components/Field';
+export { Fieldset } from './components/Fieldset';
+export { Menu } from './components/Menu';
+export { Meter } from './components/Meter';
+export { Pagination } from './components/Pagination';
+export { Progress } from './components/Progress';
+export { Radio } from './components/Radio';
+export { Select } from './components/Select';
+export { Table } from './components/Table';
+export { Tabs } from './components/Tabs';
+export { Toast } from './components/Toast';
+export { Tooltip } from './components/Tooltip';
+
+// Simple components (direct exports)
+export { ActionBar, ActionBarLabel, ActionBarActions } from './components/ActionBar';
+export type { ActionBarProps, ActionBarLabelProps, ActionBarActionsProps } from './components/ActionBar';
+
+export { Alert, type AlertProps } from './components/Alert';
+
+export {
+  Avatar,
+  AvatarRoot,
+  AvatarImage,
+  AvatarFallback,
+  type AvatarRootProps,
+  type AvatarImageProps,
+  type AvatarFallbackProps,
+  type AvatarSize,
+  type AvatarVariant,
+  type AvatarColor,
+} from './components/Avatar';
+
+export { Badge, type BadgeProps, type BadgeVariant } from './components/Badge';
+
+export { Button } from './components/Button';
+export type { ButtonProps } from './components/Button';
+
+export { Chip, ChipFilter } from './components/Chip';
+export type { ChipProps, ChipFilterProps } from './components/Chip';
+
+export { Form } from './components/Form';
+export type { FormProps } from './components/Form';
+
+export { CentralIcon } from './components/Icon';
+export type { CentralIconName, CentralIconProps } from './components/Icon';
+
+export { Input, type InputProps } from './components/Input';
+
+export { Loader } from './components/Loader';
+export type { LoaderProps } from './components/Loader';
+
+export { Separator } from './components/Separator';
+export type { SeparatorProps } from './components/Separator';
+
+export { Shortcut, type ShortcutProps } from './components/Shortcut';
+
+export { Switch } from './components/Switch';
+export type { SwitchProps } from './components/Switch';
